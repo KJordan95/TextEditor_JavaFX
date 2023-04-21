7cff152182c2690b1fb8a4fbb97dee1279e3ae3e
KJordan95 <kameronjordan95@gmail.com>
2023-04-21 15:09:26 -0500

    added javafx modules to batch file

--- run.bat
+++ run.bat
@@ -1,2 +1,5 @@
-javac SpellCheckerWindow.java SpellChecker.java
-java SpellCheckerWindow
+@REM replace YOUR_PATH_TO_JAVAFX_LIB with the actual file path. For example:
+@REM set PATH_TO_FX="C:\Program Files\Java\javafx-sdk-20.0.1\lib"
+set PATH_TO_FX="YOUR_PATH_TO_JAVAFX_LIB"
+javac --module-path %PATH_TO_FX% --add-modules javafx.controls SpellCheckerWindow.java SpellChecker.java
+java --module-path %PATH_TO_FX% --add-modules javafx.controls SpellCheckerWindow
